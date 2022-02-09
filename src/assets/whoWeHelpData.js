a36b3d5a6fd00ff25af7ce7fad99e370461fa96a
gsajdok <grzegorzsajdok@outlook.com>
2022-02-09 11:28:08 +0100

    Finished basic functionality of the list

---
+++ src/assets/whoWeHelpData.js
@@ -0,0 +1,92 @@
+export const WhoWeHelpData = [
+    {
+        description: "Fundacje! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+        list: [
+            {
+                name: "Organizacja 1",
+                description: "Opis organizacji 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 2",
+                description: "Opis organizacji 2",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 3",
+                description: "Opis organizacji 3",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 4",
+                description: "Opis organizacji 4",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 5",
+                description: "Opis organizacji 5",
+                examples: "Costam, cos, takze cos"
+            }
+        ]
+    },
+    {
+        description: "Pozarzodawe! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+        list: [
+            {
+                name: "Organizacja 1",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 2",
+                description: "Lokalna 2",
+                examples: "Costam, cos, takze cos"
+            }
+        ]
+    },
+    {
+        description: "Lokanle! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+        list: [
+            {
+                name: "Organizacja 1",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 5",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 15",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 25",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 777",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 767",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 13",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+            {
+                name: "Organizacja 111",
+                description: "Lokalna 1",
+                examples: "Costam, cos, takze cos"
+            },
+        ]
+    }
+]
--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -1,7 +1,58 @@
+import Decoration from "../assets/Decoration.svg";
+import {HomeWhoWeHelpResult} from "./HomeWhoWeHelpResult";
+import {WhoWeHelpData} from "../assets/whoWeHelpData";
+import {useEffect, useState} from "react";
+
 export const HomeWhoWeHelp = ({id}) => {
+    const [currentData, setCurrentData] = useState(WhoWeHelpData[0]);
+    const [page, setPage] = useState(1);
+
+    const buttonClickHandler = (e) => {
+        if(e.target.id === 'option1') {
+            setCurrentData(WhoWeHelpData[0]);
+        } else if(e.target.id === 'option2') {
+            setCurrentData(WhoWeHelpData[1]);
+        } else if(e.target.id === 'option3') {
+            setCurrentData(WhoWeHelpData[2]);
+        }
+        setPage(1);
+    }
+
+    const pageClickHandler = e => {
+        setPage(parseInt(e.target.id))
+    }
+
+
     return (
-        <section id={id}>
-            <h1>Who we help</h1>
+        <section className="homeWhoWeHelp" id={id}>
+            <div className="wrapper">
+                <div className="content">
+                    <div className="header">
+                        <h2>Komu pomagamy?</h2>
+                        <img src={Decoration} alt="Decoration" className="decoration"/>
+                    </div>
+                    <div className="whoWeHelp__functionality">
+                        <div className="button__wrapper">
+                            <button onClick={buttonClickHandler} className="button" id="option1">Fundacjom</button>
+                            <button onClick={buttonClickHandler} className="button" id="option2">Organizacjom pozarządowym</button>
+                            <button onClick={buttonClickHandler} className="button" id="option3">Lokalnym zbiórkom</button>
+                        </div>
+                        <div className="description__wrapper">
+                            <p>{currentData.description}</p>
+                        </div>
+                        <div className="results__wrapper">
+                            <div className="result">
+                                {currentData.list.filter((e,i) => i >= (page-1)*3 && i <= ((page)*3)-1).map(e => (<HomeWhoWeHelpResult data={e} key={e.name} page={page}/>))}
+                            </div>
+                        </div>
+                        <div className="controller_wrapper">
+                            {Array(Math.ceil(currentData.list.length/3)).fill(null).map((e, i) => (
+                                <button onClick={pageClickHandler} className="button button--small" id={i+1} key={i}>{i+1}</button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
