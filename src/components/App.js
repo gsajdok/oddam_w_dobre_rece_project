65923de3a80791f2a97ff8debc29d669138bc7eb
gsajdok <grzegorzsajdok@outlook.com>
2022-02-10 20:33:17 +0100

    Fully realized the navigation functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,33 @@ import {NotFound} from "./NotFound";
 import {LoginPage} from "./Login";
 import {RegisterPage} from "./RegisterPage";
 import {Navigation} from "./Navigation";
+import {scroller} from "react-scroll";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [scrollTarget, setScrollTarget] = useState(undefined);
+
+    const scrollFunction = (id) => {
+        scroller.scrollTo(id, {
+            duration: 500,
+            delay: 0,
+            smooth: "easeInOutQuart"}
+        )
+    }
+
+    useEffect(() => {
+        if(scrollTarget !== undefined) {
+            scroller.scrollTo(scrollTarget, {
+                duration: 500,
+                delay: 0,
+                smooth: "easeInOutQuart"});
+            setScrollTarget(undefined);
+        }
+    })
+
   return (
     <BrowserRouter>
-        <Navigation/>
+        <Navigation scrollFunction={(id) => scrollFunction(id)} setScrollTarget={(id) => setScrollTarget(id)}/>
         <Routes>
             <Route path="*" element={<NotFound/>}/>
             <Route path="/" element={<Home/>}/>
