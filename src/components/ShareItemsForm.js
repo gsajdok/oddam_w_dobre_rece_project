1d5b8bdad87011b01e22cf9b041f13d14a822458
gsajdok <grzegorzsajdok@outlook.com>
2022-02-21 14:43:38 +0100

    Added validation

--- src/components/ShareItemsForm.js
+++ src/components/ShareItemsForm.js
@@ -1,41 +1,24 @@
 import {ShareItemsContext} from "../contextAPI/shareItemsContext";
 import {useContext, useMemo, useState} from "react";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../helpers/firebase";
+import { getFirestore } from 'firebase/firestore'
+import { doc, setDoc } from "firebase/firestore";
+
 import {StepOne} from "./ShareItemsFormSteps/StepOne";
 import {StepTwo} from "./ShareItemsFormSteps/StepTwo";
 import {StepThree} from "./ShareItemsFormSteps/StepThree";
 import {StepFour} from "./ShareItemsFormSteps/StepFour";
-import { get, getDatabase, ref, set } from "firebase/database";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "../helpers/firebase";
 import {Summary} from "./ShareItemsFormSteps/Summary";
 import {ThankYou} from "./ShareItemsFormSteps/ThankYou";
-import { collection, addDoc } from "firebase/firestore";
-import { getFirestore } from 'firebase/firestore'
-import { doc, setDoc } from "firebase/firestore";
 
 export const ShareItemsForm = () => {
     const {step, setStep, formData} = useContext(ShareItemsContext);
     const [user] = useAuthState(auth)
+    const [isError, setError] = useState(false);
 
     const sendData = () => {
         const db = getFirestore();
-        // const db = getDatabase();
-        // set( ref( db, 'submittedForms/' + user.uid + '/' + Date.now()), {
-        //     formData
-        // }).then(() => {
-        //     console.log("Success!")
-        //     setStep(6);
-        // }).catch((error) => {
-        //     console.log(error)
-        // })
-
-        // addDoc(collection(db, "forms"), formData).then(() => {
-        //     console.log("Success!")
-        //     setStep(6);
-        // }).catch((error) => {
-        //     console.log(error)
-        // })
-
         const timestamp = Date.now().toString();
 
         setDoc(doc(db, `submittedData/forms/${user.uid}`, timestamp), {...formData, timestamp: Date.now()}).then(() => {
@@ -47,6 +30,22 @@ export const ShareItemsForm = () => {
 
     }
 
+    const validateFormOnStep = (stepNumber) => {
+        setError(false);
+        if(stepNumber===1) {
+            if(formData.type===0) {setError(true); return false;}
+        } else
+        if(stepNumber===2) {
+            if(formData.amount===0) {setError(true); return false;}
+        } else
+        if(stepNumber===3) {
+            if(formData.localisation===0 || formData.helpGroups.indexOf(true)===-1) {setError(true); return false;}
+        } else
+        if(stepNumber===4) {
+            if(Object.entries(formData.address).indexOf("")!==-1 || formData.collection.date==="" || formData.collection.hour==="") {setError(true); return false;}
+        }
+        return true;
+    }
 
     const stepHandlerSwitch = (id) => {
         switch(id) {
@@ -68,7 +67,9 @@ export const ShareItemsForm = () => {
     }
 
     const nextButtonHandler = () => {
-        setStep(step+1);
+        if(validateFormOnStep(step)) {
+            setStep(step+1);
+        }
     }
 
     const prevButtonHandler = () => {
@@ -89,11 +90,15 @@ export const ShareItemsForm = () => {
                     </div>
                     {stepHandlerSwitch(step)}
                     <div className="errorWrapper">
+                        {isError && <h2 style={{color: "red"}}>Prosze wypełnić wszystkie wymagane pola!</h2>}
                     </div>
                     <div className="buttonWrapper">
                         {step>1 && step<=5 && <button onClick={prevButtonHandler} className="button button--active">Wstecz</button>}
                         {step<5 && <button onClick={nextButtonHandler} className="button button--active">Dalej</button>}
                         {step===5 && <button onClick={sendData} className="button button--active">Potwierdzam</button>}
+                        <button onClick={() => console.log(formData)}>form data</button>
+                        <button onClick={() => console.log(isError)}>is error</button>
+                        <button onClick={() => console.log(step)}>step</button>
                     </div>
                 </div>
             </div>
