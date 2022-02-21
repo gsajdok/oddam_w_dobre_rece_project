3e3c1c5a1c63fdc7c5b5541914646b9a96cefe9c
gsajdok <grzegorzsajdok@outlook.com>
2022-02-21 08:54:44 +0100

    Polished Step Three

--- src/components/ShareItemsFormSteps/StepThree.js
+++ src/components/ShareItemsFormSteps/StepThree.js
@@ -4,7 +4,7 @@ import {ShareItemsContext} from "../../contextAPI/shareItemsContext";
 export const StepThree = () => {
     const {formData, setFormData} = useContext(ShareItemsContext);
 
-    const onChangeValue = (e) => {
+    const onChangeValueSelect = (e) => {
         if(e.target.name==="localisation") {
             setFormData(prevState => ({
                 ...prevState,
@@ -13,7 +13,7 @@ export const StepThree = () => {
         }
     }
 
-    const onClickValue = (e) => {
+    const onClickValueButton = (e) => {
         let optionsArrayCopy = formData.helpGroups;
         optionsArrayCopy.splice(parseInt(e.target.value), 1, !optionsArrayCopy[parseInt(e.target.value)]);
         setFormData(prevState => ({
@@ -22,7 +22,7 @@ export const StepThree = () => {
         }));
     }
 
-    const textInputChangeHandler = (e) => {
+    const onChangeValueInput = (e) => {
         setFormData(prevState => ({
             ...prevState,
             groupName: e.target.value
@@ -32,31 +32,33 @@ export const StepThree = () => {
     return (
         <div className="formWrapper">
             <h2>Lokalizacja:</h2>
-            <form id="form">
-                <label>
-                    <select style={{marginLeft: 0}} name="localisation" onChange={onChangeValue} value={formData.localisation}>
-                        <option value="0" disabled hidden>--- wybierz ---</option>
-                        <option value="1">Poznań</option>
-                        <option value="2">Warszawa</option>
-                        <option value="3">Kraków</option>
-                        <option value="4">Wrocław</option>
-                        <option value="5">Katowice</option>
-                    </select>
-                </label>
-            </form>
+            <div className="selectForm">
+                <form>
+                    <label>
+                        <select style={{marginLeft: 0}} name="localisation" onChange={onChangeValueSelect} value={formData.localisation}>
+                            <option value="0" disabled hidden>--- wybierz ---</option>
+                            <option value="1">Poznań</option>
+                            <option value="2">Warszawa</option>
+                            <option value="3">Kraków</option>
+                            <option value="4">Wrocław</option>
+                            <option value="5">Katowice</option>
+                        </select>
+                    </label>
+                </form>
+            </div>
             <div className="helpGroupsForm">
-                <h2>Komu chcesz pomóc?</h2>
+                <h3>Komu chcesz pomóc?</h3>
                 <div className="helpGroupsButtons">
-                    <button value="0" className={`button button--active ${formData.helpGroups[0] && "button--toggled"}`} onClick={onClickValue}>dzieciom</button>
-                    <button value="1" className={`button button--active ${formData.helpGroups[1] && "button--toggled"}`} onClick={onClickValue}>samotnym matkom</button>
-                    <button value="2" className={`button button--active ${formData.helpGroups[2] && "button--toggled"}`} onClick={onClickValue}>bezdomnym</button>
-                    <button value="3" className={`button button--active ${formData.helpGroups[3] && "button--toggled"}`} onClick={onClickValue}>niepełnosprawnym</button>
-                    <button value="4" className={`button button--active ${formData.helpGroups[4] && "button--toggled"}`} onClick={onClickValue}>osobom starszym</button>
+                    <button value="0" className={`button button--active ${formData.helpGroups[0] && "button--toggled"}`} onClick={onClickValueButton}>dzieciom</button>
+                    <button value="1" className={`button button--active ${formData.helpGroups[1] && "button--toggled"}`} onClick={onClickValueButton}>samotnym matkom</button>
+                    <button value="2" className={`button button--active ${formData.helpGroups[2] && "button--toggled"}`} onClick={onClickValueButton}>bezdomnym</button>
+                    <button value="3" className={`button button--active ${formData.helpGroups[3] && "button--toggled"}`} onClick={onClickValueButton}>niepełnosprawnym</button>
+                    <button value="4" className={`button button--active ${formData.helpGroups[4] && "button--toggled"}`} onClick={onClickValueButton}>osobom starszym</button>
                 </div>
             </div>
             <div className="customGroupForm">
-                <h2>Wpisz nazwę konkretnej organizacji (opcjonalnie)</h2>
-                <input type="text" value={formData.groupName} onChange={textInputChangeHandler}/>
+                <h3>Wpisz nazwę konkretnej organizacji (opcjonalnie)</h3>
+                <input type="text" value={formData.groupName} onChange={onChangeValueInput}/>
             </div>
         </div>
     )
