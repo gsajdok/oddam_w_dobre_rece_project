abd6f420fee384a1dce74b73af01093b6b90401b
gsajdok <grzegorzsajdok@outlook.com>
2022-02-21 09:49:29 +0100

    Initial auth reworks

--- src/components/ShareItems.js
+++ src/components/ShareItems.js
@@ -1,8 +1,20 @@
 import {HomeContact} from "./HomeContact";
 import {ShareItemsHeader} from "./ShareItemsHeader";
 import {ShareItemsBody} from "./ShareItemsMainBody";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../helpers/firebase";
+import {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const ShareItems = () => {
+    const [user] = useAuthState(auth)
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if(user===null) {
+            navigate('/')
+        }
+    })
 
     return (
         <>
